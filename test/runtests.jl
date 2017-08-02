609f5a70f13886000f07ff8145b8e9dc329f28e6
Andreas Noack <andreasnoackjensen@gmail.com>
2017-08-02 10:54:07 -0400

    Update README and overwrite Polynomials.roots

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,20 +2,20 @@ using AMVW, Base.Test, Polynomials
 
 @testset "Standard normal coefficients" begin
     p = Poly(randn(50))
-    @test sort(abs.(roots(p))) ≈ sort(abs.(AMVW.rootsAMVW(p)))
+    @test sort(abs.(roots(p))) ≈ sort(abs.(roots(p)))
 end
 @testset "Standard normal complex coefficient" begin
     p = Poly(complex.(randn(50), randn(50)))
-    @test sort(abs.(roots(p))) ≈ sort(abs.(AMVW.rootsAMVW(p)))
+    @test sort(abs.(roots(p))) ≈ sort(abs.(roots(p)))
 end
 
 @testset "Possible to calculate roots of large polynomial" begin
     p = Poly(randn(5000))
-    @time AMVW.rootsAMVW(p)
+    @time roots(p)
 end
 
 @testset "But polynomial root finding is ill conditioned" begin
     rts = 1:100.0
     p = mapreduce(t -> Poly([1, -t]), *, Poly(Float64[1]), rts)
-    @show norm(sort(abs.(AMVW.rootsAMVW(p))) - rts)
+    @show norm(sort(abs.(roots(p))) - rts)
 end
