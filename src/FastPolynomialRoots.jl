dd5aec7a0c9286fa804ae535ff295055383bfc83
Andreas Noack <andreas@noack.dk>
2020-04-15 17:27:41 +0200

    Update to Julia 1.0

--- src/FastPolynomialRoots.jl
+++ src/FastPolynomialRoots.jl
@@ -2,55 +2,55 @@ module FastPolynomialRoots
 
 using Requires
 
-    const dpath = joinpath(Pkg.dir("FastPolynomialRoots"), "deps", "libamvwdouble")
-    const spath = joinpath(Pkg.dir("FastPolynomialRoots"), "deps", "libamvwsingle")
+function __init__()
 
-    function rootsFastPolynomialRoots(a::Vector{Float64})
-
-        pl    = reverse!(a[1:end - 1] ./ a[end])
-        np    = length(pl)
-        reigs = similar(pl)
-        ieigs = similar(pl)
-        its   = Vector{Int32}(np)
-        flag  = Int32[0]
-
-        ccall((:damvw_, dpath), Void,
-            (Ptr{Int32}, Ptr{Float64}, Ptr{Float64}, Ptr{Float64}, Ptr{Int32}, Ptr{Int32}),
-            &np, pl, reigs, ieigs, its, flag)
-
-        if flag[1] != 0
-            error("error code: $(flag[1])")
-        end
-        return complex.(reigs, ieigs)
+    @require Polynomials="f27b6e38-b328-58d1-80ce-0feddd5e7a45" begin
+        Polynomials.roots(p::Union{Polynomials.Poly{Float64},Polynomials.Poly{Complex{Float64}}}) = rootsFastPolynomialRoots(p.a)
+        Polynomials.roots(p::Polynomials.Poly{<:Integer}) = rootsFastPolynomialRoots(convert(Polynomials.Poly{Float64}, p))
     end
-
-    function rootsFastPolynomialRoots(a::Vector{Complex{Float64}})
-
-        pl    = reverse!(a[1:end - 1] ./ a[end])
-        plr   = real(pl)
-        pli   = imag(pl)
-        np    = length(pl)
-        reigs = similar(plr)
-        ieigs = similar(plr)
-        its   = Vector{Int32}(np)
-        flag  = Int32[0]
-
-        ccall((:zamvw_, spath), Void,
-            (Ptr{Int32}, Ptr{Float64}, Ptr{Float64}, Ptr{Float64}, Ptr{Float64}, Ptr{Int32}, Ptr{Int32}),
-            &np, plr, pli, reigs, ieigs, its, flag)
-
-        if flag[1] != 0
-            error("error code: $(flag[1])")
-        end
-        return complex.(reigs, ieigs)
-    end
-
-
-    @require Polynomials begin
-        using Polynomials: Poly
-        import Polynomials: roots
-        roots(p::Union{Poly{Float64},Poly{Complex{Float64}}}) = rootsFastPolynomialRoots(p.a)
-        roots{T<:Integer}(p::Poly{T}) = rootsFastPolynomialRoots(convert(Poly{Float64}, p))
-    end
-
 end
+
+const dpath = joinpath(@__DIR__(), "..", "deps", "libamvwdouble")
+const spath = joinpath(@__DIR__(), "..", "deps", "libamvwsingle")
+
+function rootsFastPolynomialRoots(a::Vector{Float64})
+
+    pl    = reverse!(a[1:end - 1] ./ a[end])
+    np    = length(pl)
+    reigs = similar(pl)
+    ieigs = similar(pl)
+    its   = Vector{Int32}(undef, np)
+    flag  = Int32[0]
+
+    ccall((:damvw_, dpath), Cvoid,
+        (Ref{Int32}, Ptr{Float64}, Ptr{Float64}, Ptr{Float64}, Ptr{Int32}, Ptr{Int32}),
+        np, pl, reigs, ieigs, its, flag)
+
+    if flag[1] != 0
+        error("error code: $(flag[1])")
+    end
+    return complex.(reigs, ieigs)
+end
+
+function rootsFastPolynomialRoots(a::Vector{Complex{Float64}})
+
+    pl    = reverse!(a[1:end - 1] ./ a[end])
+    plr   = real(pl)
+    pli   = imag(pl)
+    np    = length(pl)
+    reigs = similar(plr)
+    ieigs = similar(plr)
+    its   = Vector{Int32}(undef, np)
+    flag  = Int32[0]
+
+    ccall((:zamvw_, spath), Cvoid,
+        (Ref{Int32}, Ptr{Float64}, Ptr{Float64}, Ptr{Float64}, Ptr{Float64}, Ptr{Int32}, Ptr{Int32}),
+        np, plr, pli, reigs, ieigs, its, flag)
+
+    if flag[1] != 0
+        error("error code: $(flag[1])")
+    end
+    return complex.(reigs, ieigs)
+end
+
+end # module
