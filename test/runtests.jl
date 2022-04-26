3a4f1fa9e2fa3f6c42bde5f4ad9765a12bab965d
Andreas Noack <andreas@noack.dk>
2022-04-26 14:02:44 +0200

    Fit integer method and tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,12 +1,18 @@
-using Test, FastPolynomialRoots, Polynomials
+using Test, FastPolynomialRoots, Polynomials, LinearAlgebra
 
 @testset "Standard normal coefficients" begin
     p = Polynomial(randn(50))
-    @test sort(abs.(roots(p))) ≈ sort(abs.(roots(p)))
+    @test sort(abs.(roots(p))) ≈ sort(abs.(eigvals(companion(p))))
 end
-@testset "Standard normal complex coefficient" begin
+
+@testset "Standard normal complex coefficients" begin
     p = Polynomial(complex.(randn(50), randn(50)))
-    @test sort(abs.(roots(p))) ≈ sort(abs.(roots(p)))
+    @test sort(abs.(roots(p))) ≈ sort(abs.(eigvals(companion(p))))
+end
+
+@testset "Integer coefficients (Issue 19)" begin
+    p = Polynomial([1, 10, 100, 1000])
+    @test sort(abs.(roots(p))) ≈ sort(abs.(eigvals(companion(p))))
 end
 
 @testset "Large polynomial" begin
@@ -14,8 +20,9 @@ end
     @time roots(p)
 
     @info "Possible to calculate roots of large polynomial"
-    @show rts = 1:100.0
-    p = mapreduce(t -> Polynomial([1, -t]), *, rts, init=Polynomial(Float64[1]))
+    # @show λs = 1:100.0
+    λs = sort(randn(100), rev=true)
+    p = fromroots(λs)
     @info "But polynomial root finding is ill conditioned"
-    @show sum(abs2, sort(abs.(roots(p))) - rts)
+    @test sum(abs2, roots(p) - λs) < 1000
 end
